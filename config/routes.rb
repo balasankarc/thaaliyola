eead57507226783898f463d352d9e11b80e12363
Balasankar <c.balasankar@gmail.com>
2014-01-19 13:02:58 +0530

    Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
     root 'static_pages#index'
     match '/', to: 'static_pages#index', via: 'get'
+    match '/help/', to: 'static_pages#help', via: 'get'
+    get 'static_pages/help'
 end

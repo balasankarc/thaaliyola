42bee0af31390c0578fa12bdfa386dd13a785d1c
Balasankar <c.balasankar@gmail.com>
2014-05-10 15:27:53 +0530

    Cleaning

--- app/controllers/static_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class StaticPagesController < ApplicationController
-  def index
-  end
-
-  def help
-  end
-  def about
-  end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-SampleApp::Application.routes.draw do
-    root 'static_pages#index'
-    match '/', to: 'static_pages#index', via: 'get'
-    match '/help/', to: 'static_pages#help', via: 'get'
-    get 'static_pages/help'
-end

56bff852941b432b5254690285bd6ae4b1fbfc2f
Balasankar <c.balasankar@gmail.com>
2014-05-10 15:30:24 +0530

    initial commit - after cleaning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThaaliyolaRor::Application.config.secret_key_base = 'ade234362a2b01ca9625827412a16c21bddc9e87c8a95098f9570750c423571386570ef16d641ff934dd99681770bcf46398a556bef695684f771c333d421562'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThaaliyolaRor::Application.config.session_store :cookie_store, key: '_thaaliyola_ror_session'

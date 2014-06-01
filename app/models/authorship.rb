010e946df301eeea8c7828e81e8c61a8dcf212e4
Balasankar <c.balasankar@gmail.com>
2014-06-01 23:01:34 +0530

    Author Edit

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,4 +1,4 @@
 class Authorship < ActiveRecord::Base
-    belongs_to :book, :dependent => :destroy
-    belongs_to :author , :dependent => :destroy
+    belongs_to :book
+    belongs_to :author
 end

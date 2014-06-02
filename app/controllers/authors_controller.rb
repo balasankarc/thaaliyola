c1b56dd16750c2a80364baf6edce2947920cc397
Balasankar <c.balasankar@gmail.com>
2014-06-02 19:49:23 +0530

    Author Photo

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -76,6 +76,6 @@ class AuthorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def author_params
-      params.require(:author).permit(:name)
+      params.require(:author).permit(:name, :photo)
     end
 end

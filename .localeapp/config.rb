b2dd9eb5d0e47af3d4f3ec15901983c500ceb18e
Balasankar <c.balasankar@gmail.com>
2014-07-30 07:07:51 +0530

    localeapp

---
+++ .localeapp/config.rb
@@ -0,0 +1,6 @@
+Localeapp.configure do |config|
+  config.api_key                    = 'X6Zqquptmw1E1K4qnB47FJ37LbMc1qz0FhIJgJEUZn9YdvFyu7'
+  config.translation_data_directory = 'locales'
+  config.synchronization_data_file  = '.localeapp/log.yml'
+  config.daemon_pid_file            = '.localeapp/localeapp.pid'
+end
